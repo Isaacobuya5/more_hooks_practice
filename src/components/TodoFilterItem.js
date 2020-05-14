5f97d6128db72033a0b96a62e8a69e6e981738d4
Isaac Obuya <Szack4477@gmail.com>
2020-05-14 15:24:31 +0300

    convert TodoFilterItem into a function component

--- src/components/TodoFilterItem.js
+++ src/components/TodoFilterItem.js
@@ -1,28 +1,40 @@
 import React from "react";
 
-export default class TodoFilterItem extends React.Component {
+// export default class TodoFilterItem extends React.Component {
 
-    constructor(props) {
-        super(props);
-      this.handleFilter = this.handleFilter.bind(this);
+//     constructor(props) {
+//         super(props);
+//       this.handleFilter = this.handleFilter.bind(this);
+//     }
+
+//     handleFilter() {
+//         const { name, filterTodos } = this.props;
+//         filterTodos(name);
+//     }
+
+//     render() {
+//         const { name, filter='all' } = this.props;
+//         const style = {
+//             color: "blue",
+//             cursor: "pointer",
+//             fontWeight: (filter === name ) ? 'bold' : 'normal'
+//         }
+//         return (
+//         <span style={style}
+//         onClick={this.handleFilter}
+//         >{name}</span>
+//         );
+//     }
+// }
+
+const TodoFilterItem = ({name,filterTodos,filter='all' }) => {
+    const style = {
+        color: "blue",
+        cursor: "pointer",
+        fontWeight: (filter === name ) ? 'bold' : 'normal'
     }
 
-    handleFilter() {
-        const { name, filterTodos } = this.props;
-        filterTodos(name);
-    }
-
-    render() {
-        const { name, filter='all' } = this.props;
-        const style = {
-            color: "blue",
-            cursor: "pointer",
-            fontWeight: (filter === name ) ? 'bold' : 'normal'
-        }
-        return (
-        <span style={style}
-        onClick={this.handleFilter}
-        >{name}</span>
-        );
-    }
+    return <span style={style} onClick={() => filterTodos(name)}>{name}</span>
 }
+
+export default TodoFilterItem;
