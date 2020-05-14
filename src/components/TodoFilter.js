06dc03e45c9490bc78e049f8d1fa5abb404dccdd
Isaac Obuya <Szack4477@gmail.com>
2020-05-14 15:18:58 +0300

    migrate TodoFilter component to function component

--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -1,16 +1,25 @@
 import React from "react";
 import TodoFilterItem from "./TodoFilterItem";
 
-export default class TodoFilter extends React.Component {
+// export default class TodoFilter extends React.Component {
 
 
-    render() {
-        return (
-            <div>
-         <TodoFilterItem name="all" {...this.props}/>{" / "}
-         <TodoFilterItem name="active" {...this.props}/>{" / "}
-         <TodoFilterItem name="completed" {...this.props}/>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//          <TodoFilterItem name="all" {...this.props}/>{" / "}
+//          <TodoFilterItem name="active" {...this.props}/>{" / "}
+//          <TodoFilterItem name="completed" {...this.props}/>
+//             </div>
+//         )
+//     }
+// }
+const TodoFilter = (props) => (
+    <div>
+    <TodoFilterItem name="all" {...props}/>{" / "}
+    <TodoFilterItem name="active" {...props}/>{" / "}
+    <TodoFilterItem name="completed" {...props}/>
+    </div>
+);
+
+export default TodoFilter;
